8d86f3b089c14889f36ec64ebc56eb4766c23901
msuleman0100 <msuleman0100@gmail.com>
2023-04-24 14:51:45 +0500

    First Commit

---
+++ Xpensify/XpensifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpensifyApp.swift
+//  Xpensify
+//
+//  Created by Muhammad Suleman on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct XpensifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
